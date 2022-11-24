abe47d81c679285ff8033b4aab64bca454cc0348
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2022-11-24 20:05:32 +0100

    update run script

--- lab11/c/client/run.sh
+++ lab11/c/client/run.sh
@@ -1,1 +1,1 @@
-docker run -it --init --rm --network-alias z46_lab11_c_client --network z46_network --name z46_lab11_c_client z46_lab11_c_client 8080
+docker run -it --init --rm --network-alias z46_lab11_c_client --network z46_network --name z46_lab11_c_client z46_lab11_c_client $1 $2
--- lab11/c/server/run.sh
+++ lab11/c/server/run.sh
@@ -1,1 +1,1 @@
-docker run -it --init --rm --network-alias z46_lab11_c_server --network z46_network --name z46_lab11_c_server z46_lab11_c_server 8080
+docker run -it --init --rm --network-alias z46_lab11_c_server --network z46_network --name z46_lab11_c_server z46_lab11_c_server $1 
