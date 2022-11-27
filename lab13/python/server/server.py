82d87241e1cfef043729e3c987aac13b19ada74c
jjedrzej <01158710@pw.edu.pl>
2022-11-27 22:26:39 +0100

    add receiving struct in python server

---
+++ lab13/python/server/server.py
@@ -0,0 +1,55 @@
+import socket
+import argparse
+import struct
+
+BUF_SIZE = 64
+HOST_IP = ''
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+
+    server_address_port = (HOST_IP, int(args.port))
+
+    udp_server_socket = socket.socket(
+        family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    udp_server_socket.bind(server_address_port)
+
+    print(
+        f'UDP server up and listening on host {server_address_port[0]}, ' +
+        f'port {server_address_port[1]}')
+    
+    # create example struct
+    class Struct:
+        def __init__(self) -> None:
+            self.a: (int)
+            self.b: (int)
+            self.c: (str)
+    stc = Struct()
+    # format of struct - types of variables
+    struct_format = "@lh10s"
+
+    while(True):
+
+        message_address = udp_server_socket.recvfrom(BUF_SIZE)
+        message = message_address[0]
+        address = message_address[1]
+
+        if not message:
+            print("Error in datagram")
+            break
+
+        try:
+            stc.a, stc.b, stc.c = struct.unpack(struct_format, message[:20])
+        except UnicodeDecodeError:
+            print(type(message))
+
+        print(f'Client IP Address: {address}')
+        print(f'Struct received: a={stc.a}, b={stc.b}, c={stc.c}\n')
