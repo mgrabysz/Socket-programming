64c401b948777cbb624b6340d9ce4513d708728b
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2022-12-27 15:33:59 +0100

    basic registration

--- project/registration.py
+++ project/registration.py
@@ -1,2 +1,27 @@
+__registered_devices = set()
+
+
+def __register(device_id: int):
+    global __registered_devices
+    __registered_devices.add(device_id)
+    print(f"Registered device {device_id}")
+
+
+def __unregister(device_id: int):
+    global __registered_devices
+    __registered_devices.remove(device_id)
+    print(f"Unregistered device {device_id}")
+
+
+def get_registered_devices() -> set[int]:
+    global __registered_devices
+    return __registered_devices
+
+
 def handle_message(message: dict):
     print(f"handling registration message: {message}")
+
+    if message['action'] == "register":
+        __register(message['device_id'])
+    elif message['action'] == "unregister":
+        __unregister(message['device_id'])
