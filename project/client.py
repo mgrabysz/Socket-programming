82ef048ae462b54dbc8ade8682a8bfb7650dd169
Marcin Grabysz <margrabysz@gmail.com>
2022-12-27 14:27:05 +0100

    Implement single client

--- project/client.py
+++ project/client.py
@@ -1,5 +1,53 @@
-from messages import Register_message
+import random
+import socket
+import time
+
+from messages import Register_message, Transmission_message, Unregister_message
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 2137
+class Client:
+    def __init__(self, device_id, server_ip, server_port):
+        self.device_id = device_id
+        self.server_address = (server_ip, server_port)
+
+    def transmit(self, num_of_messages, interval=3):
+        """
+        :param num_of_messages: number of messages to be sent
+        :param interval: interval in seconds
+        :return:
+        """
+        client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        bytes_sent = self.send_register_message(client_socket)
+        print(f'Bytes sent (register message): {bytes_sent}')
+
+        for _ in range(num_of_messages):
+            time.sleep(interval)
+            payload = random.random()
+            bytes_sent = self.send_transmission_message(client_socket, payload)
+            print(f'Message sent: {payload}')
+            print(f'Bytes sent: {bytes_sent}')
+
+        bytes_sent = self.send_unregister_message(client_socket)
+        print(f'Bytes sent (unregister message): {bytes_sent}')
+
+    def send_register_message(self, client_socket):
+        register_message = Register_message(self.device_id, time.time())
+        msg_bytes = register_message.to_json().encode()
+        return client_socket.sendto(msg_bytes, self.server_address)
+
+    def send_unregister_message(self, client_socket):
+        unregister_message = Unregister_message(self.device_id)
+        msg_bytes = unregister_message.to_json().encode()
+        return client_socket.sendto(msg_bytes, self.server_address)
+
+    def send_transmission_message(self, client_socket, payload):
+        register_message = Transmission_message(self.device_id, time.time(), payload)
+        msg_bytes = register_message.to_json().encode()
+        return client_socket.sendto(msg_bytes, self.server_address)
+
 
 if __name__ == "__main__":
-    message = Register_message(2, 20)
-    print(message.to_json())
+
+    client = Client(1, SERVER_IP, SERVER_PORT)
+    client.transmit(10)
--- project/messages.py
+++ project/messages.py
@@ -1,10 +1,29 @@
 import json
 
 class Register_message:
-    def __init__(self, id, timestamp):
+    def __init__(self, device_id, timestamp):
         self.action = "register"
-        self.id = id
+        self.device_id = device_id
         self.timestamp = timestamp
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+class Unregister_message:
+    def __init__(self, device_id):
+        self.action = "unregister"
+        self.device_id = device_id
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+class Transmission_message:
+    def __init__(self, device_id, timestamp, payload):
+        self.device_id = device_id
+        self.timestamp = timestamp
+        self.payload = payload
 
     def to_json(self):
         return json.dumps(self.__dict__)
