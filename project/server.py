4f305a9032ebf694fbd8aa6a1e8dffeaaa743c16
Patrycja Wysocka <w512patrycja@gmail.com>
2023-01-03 17:06:43 +0100

    added servers

--- project/server.py
+++ project/server.py
@@ -1,42 +1,64 @@
 import socket
 import argparse
 import json
+from threading import Lock, Thread
+
+BUF_SIZE = 65536
+HOST_IP = ""
+PORTS = list(range(2140, 2142))
+NUM_OF_DEVICES = 2
 
 
-BUF_SIZE = 65536  # bo taki był podany w bramie
-HOST_IP = ''
+class Server:
+    def __init__(self, device_id, host_ip, port, lock):
+        self.device_id = device_id
+        self.host_ip = host_ip
+        self.port = port
+        self.lock = lock
+
+        self.server_socket = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        self.server_socket.bind((self.host_ip, self.port))
+
+        print(f'Server nr {self.device_id} up and listening on port {self.port}')
+
+    def listen(self):
+        message_bytes = self.server_socket.recv(BUF_SIZE)
+        message_json = json.loads(message_bytes)
+
+        print(f'Server nr {self.device_id} received message: {message_json}')
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("port")
-    return parser
+class Server_manager:
+    def __init__(self, ports, host_ip, num_of_devices):
+        self.ports = ports
+        self.host_ip = host_ip
+        self.num_of_devices = num_of_devices
+        self.lock = Lock()
+        self.threads = []
+    
+    def run_devices(self):
+        for device_nr, port in zip(range(self.num_of_devices), self.ports):
+            self.threads.append(Thread(target=multi_threaded_server, args=(
+                device_nr, port, self.host_ip, self.lock
+            )))
+        
+        for thread in self.threads:
+            thread.start()
+
+        for thread in self.threads:
+            thread.join()
+
+
+def multi_threaded_server(device_id, port, host_ip, lock):
+    server = Server(device_id, host_ip, port, lock)
+
+    while True:
+        server.listen()
 
 
 if __name__ == "__main__":
-    parser = create_parser()
-    args = parser.parse_args()
-
-    server_address_port = (HOST_IP, int(args.port))
-
-    udp_server_socket = socket.socket(
-        family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udp_server_socket.bind(server_address_port)
-
-    print(
-        f'UDP server up and listening on host {server_address_port[0]}, ' +
-        f'port {server_address_port[1]}')
-
-    while(True):
-        message_bytes = udp_server_socket.recvfrom(BUF_SIZE)
-        message = message_bytes[0]
-        address = message_bytes[1]
-
-        if not message:
-            print("Error in datagram")
-            break
-
-        message_json = json.loads(message_bytes)
-
-        print(f'Client IP Address: {address}')
-        print(f'Client message: {message_json}')
+    
+    server_manager = Server_manager(PORTS, HOST_IP, NUM_OF_DEVICES)
+    server_manager.run_devices()
