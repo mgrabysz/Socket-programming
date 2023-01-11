6a6764813da9cd8250e2092eea092cb60c2545f1
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2023-01-11 14:44:08 +0100

    refactor server: use CamelCase for classes, add main()

--- project/server/server.py
+++ project/server/server.py
@@ -1,5 +1,4 @@
 import socket
-import argparse
 import json
 from threading import Lock, Thread
 
@@ -16,8 +15,7 @@ class Server:
         self.port = port
         self.lock = lock
 
-        self.server_socket = socket.socket(
-            family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         self.server_socket.bind((self.host_ip, self.port))
 
@@ -30,20 +28,20 @@ class Server:
         print(f'Server nr {self.device_id} received message: {message_json}')
 
 
-class Server_manager:
+class ServerManager:
     def __init__(self, ports, host_ip, num_of_devices):
         self.ports = ports
         self.host_ip = host_ip
         self.num_of_devices = num_of_devices
         self.lock = Lock()
         self.threads = []
-    
+
     def run_devices(self):
         for device_nr, port in zip(range(self.num_of_devices), self.ports):
             self.threads.append(Thread(target=multi_threaded_server, args=(
                 device_nr, port, self.host_ip, self.lock
             )))
-        
+
         for thread in self.threads:
             thread.start()
 
@@ -58,7 +56,10 @@ def multi_threaded_server(device_id, port, host_ip, lock):
         server.listen()
 
 
-if __name__ == "__main__":
-    
-    server_manager = Server_manager(PORTS, HOST_IP, NUM_OF_DEVICES)
+def main():
+    server_manager = ServerManager(PORTS, HOST_IP, NUM_OF_DEVICES)
     server_manager.run_devices()
+
+
+if __name__ == "__main__":
+    main()
