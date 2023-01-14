2010e3a43521f86bc64e5a3f976e85141972f82c
jjedrzej <01158710@pw.edu.pl>
2023-01-14 23:40:25 +0100

    handle incorrect registration, unregistration

--- project/gateway/registration.py
+++ project/gateway/registration.py
@@ -3,14 +3,22 @@ _registered_devices = set()
 
 def _register(device_id: int):
     global _registered_devices
+    number_of_devices = len(_registered_devices)
     _registered_devices.add(device_id)
-    print(f"Registered device {device_id}")
+    if (number_of_devices == len(_registered_devices)):
+        print(f"Devce with id={device_id} already registered, can not be registered!")
+    else:
+        print(f"Registered device {device_id}")
 
 
 def _unregister(device_id: int):
     global _registered_devices
-    _registered_devices.remove(device_id)
-    print(f"Unregistered device {device_id}")
+    number_of_devices = len(_registered_devices)
+    _registered_devices.discard(device_id)
+    if (number_of_devices == len(_registered_devices)):
+        print(f"Device with id={device_id} not registered, can not be unregistered!")
+    else:
+        print(f"Unregistered device {device_id}")
 
 
 def get_registered_devices() -> set[int]:
